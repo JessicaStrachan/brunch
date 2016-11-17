0b1c9d5ac6219b605fd21422fb92a5ebc133a7fb
JessicaStrachan <jessica.strachan@1minus1.com>
2016-11-17 07:51:39 +0000

    Google Maps (#14)

--- app/initialize.js
+++ app/initialize.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	var $ = require('jquery');
 	console.log('jQuery Loaded', $('body'));
 });
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
